b8832b7fad3f44ed5baf387209e97b44b0c08368
Chandler Mayo <chandler@chandlermayo.com>
2023-08-27 17:30:12 -0400

    Added project fles

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/chandler767/To-Do-App-Go-Postgres
+
+go 1.18
+
+require github.com/jinzhu/gorm v1.9.16
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	tpl = template.Must(template.ParseFiles("task-entry.html"))
+	db  *gorm.DB
+)
+
+type newTask struct {
+	gorm.Model
+	Task string
+}
+
+func newEntry(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		task := req.FormValue("task")
+		if task != "" {
+			db.Create(&newTask{Task: task}) // Add to database
+			err := tpl.ExecuteTemplate(w, "task-entry.html", "")
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				log.Fatalln(err)
+			}
+			return
+		}
+	}
+	err := tpl.ExecuteTemplate(w, "task-entry.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	var err error
+	db, err = gorm.Open("postgres", "host="+os.Getenv("DB_HOST")+" port="+os.Getenv("DB_PORT")+" user="+os.Getenv("DB_USER")+" dbname="+os.Getenv("DB_NAME")+" sslmode=disable password="+os.Getenv("DB_PASSWORD"))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&newTask{}) // Creates our task table
+	http.HandleFunc("/", newEntry)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
+}
